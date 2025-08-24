c4ca377221e50876077ca10d15581626c67b0ae3
Vladislav Vaganov <mr.vlad0009@gmail.com>
2025-08-24 22:02:31 +0300

    done2.0

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -11,26 +11,35 @@ class RegistrationPage:
         browser.element('#lastName').type(user.last_name)
         browser.element('#userEmail').type(user.email)
 
-
         browser.all('[for^=gender-radio]').element_by(have.text(user.gender)).click()
-
         browser.element('#userNumber').type(user.mobile)
 
 
-        browser.execute_script('document.querySelector("#fixedban")?.remove()')
-        browser.execute_script('document.querySelector("footer")?.remove()')
-
+        browser.driver.execute_script('document.querySelector("#fixedban")?.remove()')
+        browser.driver.execute_script('document.querySelector("footer")?.remove()')
 
         browser.element('#submit').perform(command.js.scroll_into_view)
         browser.element('#submit').perform(command.js.click)
 
         return self
+
     def should_have_registered(self, user):
-        browser.element('.modal-content').should(
-            have.texts(
-                f'{user.first_name} {user.last_name}',
-                user.email,
-                user.mobile
-            )
+
+        rows = browser.all('.modal-content tr')
+
+        rows.element_by(have.text('Student Name')).should(
+            have.text(f'{user.first_name} {user.last_name}')
         )
+        rows.element_by(have.text('Student Email')).should(
+            have.text(user.email)
+        )
+        rows.element_by(have.text('Gender')).should(
+            have.text(user.gender)
+        )
+        rows.element_by(have.text('Mobile')).should(
+            have.text(user.mobile)
+        )
+
+
+
         return self
